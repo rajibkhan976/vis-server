668134c4cb1524c03feda7fe27b7b9f33b67fa55
unknown <rajibkhan976@gmail.com>
2019-06-28 23:59:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const routes = require("./routes");
+
+const db = require("./models");
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use((req, res, next) => {
+  req.models = db.models;
+  next();
+});
+
+app.use('/', routes);
+
+db.connectDB().then(() => {
+  const listener = app.listen( port, () => {
+    console.info(`Server is listening on port ${port}.`);
+  });
+}).catch((error) => {
+  console.error(error);
+});
